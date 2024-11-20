4f019810e848f4b2cb49b1d957340caaaf6328bf
luisglopezt4 <125174536+luisglopezt4@users.noreply.github.com>
2024-11-19 17:20:17 -0700

    ok

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
-// script.js
-document.querySelector('form').addEventListener('submit', (e) => {
-    e.preventDefault();
-    alert('Formulario enviado');
-});
+document.addEventListener('DOMContentLoaded', function () {
+    const introText = document.querySelector('.intro-content');
+    introText.style.opacity = 0;
+    introText.style.transform = 'translateY(20px)';
+    setTimeout(() => {
+      introText.style.transition = 'opacity 1s, transform 1s';
+      introText.style.opacity = 1;
+      introText.style.transform = 'translateY(0)';
+    }, 300);
+  });
